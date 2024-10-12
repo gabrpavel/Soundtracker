cdaa4b4d997e28ce112a155c7b6e8e5e33ed7d28
gabrpavel <gabrpavel@gmail.com>
2024-10-13 01:34:16 +0300

    add deploy workflow

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,14 +1,14 @@
 // app.module.ts
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from "./app-routing.module";
-import {MovieDetailsComponent} from "./components/movie/movie-details/movie-details.component";
-import {MovieListComponent} from "./components/movie/movie-list/movie-list.component";
-import {ScreenshotDialogComponent} from "./components/movie/screenshot-dialog/screenshot-dialog.component";
-import {NgOptimizedImage} from "@angular/common";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { MovieDetailsComponent } from "./components/movie/movie-details/movie-details.component";
+import { MovieListComponent } from "./components/movie/movie-list/movie-list.component";
+import { ScreenshotDialogComponent } from "./components/movie/screenshot-dialog/screenshot-dialog.component";
+import { NgOptimizedImage } from "@angular/common";
 
 @NgModule({
   imports: [
